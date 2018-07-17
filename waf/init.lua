01c865c45251c963fa11ac6455daec4c0a97f7b3
Diego Blanco <diego.blanco@treitos.com>
2018-07-17 22:50:50 +0200

    Fix clearing the body when no body was read

--- waf/init.lua
+++ waf/init.lua
@@ -75,7 +75,10 @@ function block( rule_type, rule_id, text_to_check )
   log_blocked( rule_type, rule_id, text_to_check )
   -- clean the request to avoid rechecking it
   ngx.req.set_method( ngx.HTTP_GET )
-  ngx.req.set_body_data( '' )
+  if ngx.header.content_length ~= nil then
+    ngx.req.set_body_data( '' )
+    ngx.req.clear_header( 'Content-Length' )
+  end
   ngx.req.clear_header( 'User-Agent' )
   ngx.req.clear_header( 'Cookie' )
   -- block the request (also changes the url and clears the args)
